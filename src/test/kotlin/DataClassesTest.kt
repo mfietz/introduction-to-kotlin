ac4ff1e9f12cb2c8d21fddbc89afd4f6a6f71a82
Martin Fietz <martin.fietz@gmail.com>
2017-06-25 19:20:27 +0200

    Improve data classes test

---
+++ src/test/kotlin/DataClassesTest.kt
@@ -0,0 +1,47 @@
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class DataClassesTest {
+
+    private val me = Person("Martin", 31)
+    private val alsoMe = Person("Martin", 31)
+
+    @Test
+    fun toStringIsGenerated() {
+        assertThat(me.toString(), equalTo("Person(name=Martin, age=31)"))
+    }
+
+    @Test
+    fun equalToIsGenerated() {
+        assertThat(me, equalTo(alsoMe))
+    }
+
+    @Test
+    fun hashCodeIsGenerated() {
+        assertThat(me.hashCode(), equalTo(alsoMe.hashCode()))
+    }
+
+    @Test
+    fun copyIsGenerated() {
+        assertThat(me, equalTo(me.copy()))
+        assertThat(me.copy(name = "Horst"), not(equalTo(me)))  // copy with altered properties
+    }
+
+    @Test
+    fun componentFunctionsAreGenerated() {
+        assertThat(me.component1(), equalTo("Martin"))
+        assertThat(me.component2(), equalTo(31))
+
+        val (name, alter) = me // destructuring declaration
+        assertThat(name, equalTo("Martin"))
+        assertThat(alter, equalTo(31))
+    }
+
+}
+
+private data class Person(
+        val name: String,
+        var age: Int? = null
+)
