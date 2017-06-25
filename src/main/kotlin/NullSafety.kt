5b037fee27c20383e22409a3c4e12bf1604a8944
Martin Fietz <martin.fietz@gmail.com>
2017-06-25 19:14:26 +0200

    Move codebase to gradle

--- src/main/kotlin/NullSafety.kt
+++ src/main/kotlin/NullSafety.kt
@@ -2,9 +2,9 @@
 fun main(args: Array<String>) {
     var nullSafename: String = "Kotlin"
     nullSafename.length // guaranteed to not cause an NPE
-    nullSafename = null // compilation error
+    // nullSafename = null // compilation error
 
     var nullableName: String? = "Kotlin"
-    nullableName.length // compilation error
+    //nullableName.length // compilation error
     nullableName = null
 }
