6b9c5c9997716e26e9c425b6b45950b69dcb6b1f
Martin Fietz <martin.fietz@gmail.com>
2017-06-26 08:43:46 +0200

    Add data types

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,27 @@
+val int: Int = 42
+
+val long: Long = 42L
+
+val double: Double = 42.17
+
+val float: Float = 42.17F
+
+var hexadecimal: Int = 0xFF
+
+var binary: Int = 0b010101
+
+val isTrue: Boolean = 42 > 17
+
+val character: Char = 'c'
+
+val string: String = "this is a sentence"
+
+val rawString: String = """{
+
+"json": "oh yeah"
+
+}"""
+
+val array = arrayOf(17, 42)
+
+val array2: Array<Int> = arrayOf(17, 42)
