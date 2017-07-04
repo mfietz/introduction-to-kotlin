254494cf0219e5a09e9ef66c97116779647fb3b4
Martin Fietz <martin.fietz@gmail.com>
2017-07-04 20:21:15 +0200

    Fix typo

--- kotlin-examples/src/main/kotlin/FirstTaste.kt
+++ kotlin-examples/src/main/kotlin/FirstTaste.kt
@@ -5,7 +5,7 @@ data class Person @JvmOverloads constructor(
 
 fun main(args: Array<String>) {
     val people = listOf(Person("Alice"), Person("Bob", age = 42))
-    println("The oldest person is ${people.oldest()}}")
+    println("The oldest person is ${people.oldest()}")
 }
 
 private fun List<Person>.oldest() = this.maxBy { it.age ?: 0 }
