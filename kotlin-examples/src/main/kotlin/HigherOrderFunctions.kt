47b646f4f3b980d7415974d6ebe833814a6da6e3
Martin Fietz <martin.fietz@gmail.com>
2017-07-04 08:35:05 +0200

    Add higher order functions

---
+++ kotlin-examples/src/main/kotlin/HigherOrderFunctions.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    apply(mult(), 2, 3)
+
+}
+
+fun apply(operation: (Int, Int) -> Int, first: Int, second: Int): Int {
+    return operation(first, second)
+}
+
+fun mult(): (Int, Int) -> Int = { x, y -> x + y }
