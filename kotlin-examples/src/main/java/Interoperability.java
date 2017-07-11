73573efa2a042744364c8fa359c7c0e24635524c
Martin Fietz <martin.fietz@gmail.com>
2017-07-11 21:57:18 +0200

    Testing java list return type

--- kotlin-examples/src/main/java/Interoperability.java
+++ kotlin-examples/src/main/java/Interoperability.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Interoperability {
 
     public static void main(String[] args) {
         Person p = new Person("Martin");
         Person p2 = new Person("Martin", 31);
+    }
+
+    public static List<String> mutableList() {
+        return new ArrayList<String>();
     }
 
 }
--- kotlin-examples/src/main/kotlin/Interoperability.kt
+++ kotlin-examples/src/main/kotlin/Interoperability.kt
@@ -3,7 +3,11 @@ class InteroperabilityKt {
     fun accessMap() {
         val list = ArrayList<String>()
         val value: String? = list.get(0)
+    }
 
+    fun accessMutableList() {
+        val mutableList: List<String> = Interoperability.mutableList()
+        val mutableList2: MutableList<String> = Interoperability.mutableList()
     }
 
 }
